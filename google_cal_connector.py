c19c10c10d0fec1d2b978fd7639174e17ead39df
hannes <contact@hannesschniz.de>
2025-02-06 17:22:40 +0100

    first tests with google calendar api

--- google_cal_connector.py
+++ google_cal_connector.py
@@ -5,6 +5,7 @@
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from datetime import datetime, timedelta
 import pickle
 import os.path
 
@@ -16,7 +17,8 @@ class googleCalCon:
     
     def __init__(self):
         self.authenticate()
-        self.service = build('calendar', 'v3', credentials=creds)
+        self.service = build('calendar', 'v3', credentials=self.creds)
+        self.createEntry()
     
     def authenticate(self):
         # The file token.pickle stores the user's access and refresh tokens, and is
@@ -35,4 +37,31 @@ class googleCalCon:
                 self.creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
+                pickle.dump(self.creds, token)
+    
+    
+    def createEntry(self):
+        # Feature 2: Create a new calendar
+        new_calendar = {
+            'summary': 'New Python Calendar',
+            'timeZone': 'America/Los_Angeles'
+        }
+        created_calendar = self.service.calendars().insert(body=new_calendar).execute()
+        print(f"Created calendar: {created_calendar['id']}")
+        event = {
+            'summary': 'Python Meeting',
+            'location': '800 Howard St., San Francisco, CA 94103',
+            'description': 'A meeting to discuss Python projects.',
+            'start': {
+                'dateTime': (datetime.utcnow() + timedelta(days=1)).isoformat(),
+                'timeZone': 'America/Los_Angeles',
+            },
+            'end': {
+                'dateTime': (datetime.utcnow() + timedelta(days=1, hours=1)).isoformat(),
+                'timeZone': 'America/Los_Angeles',
+            },
+        }
+        created_event = self.service.events().insert(calendarId=created_calendar['id'], body=event).execute()
+        print(f"Created event: {created_event['id']}")
+        
+        
