39563896b39dcf4143d74689b07190578d507e0e
hannes <contact@hannesschniz.de>
2025-02-06 16:43:26 +0100

    fixed scpoes error

--- google_cal_connector.py
+++ google_cal_connector.py
@@ -31,7 +31,7 @@ class googleCalCon:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
+                    'credentials.json', self.SCOPES)
                 self.creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
