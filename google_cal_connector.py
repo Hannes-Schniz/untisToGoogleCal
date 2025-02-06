fa6f42767a4344b3e9c4622b3679d54d84f0095c
hannes <contact@hannesschniz.de>
2025-02-06 16:40:45 +0100

    created google cal connector

---
+++ google_cal_connector.py
@@ -0,0 +1,38 @@
+
+#Create Google Project and Google Calendar API key
+#add credentials.json
+#pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import pickle
+import os.path
+
+class googleCalCon:
+    
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    creds = None
+    service = None
+    
+    def __init__(self):
+        self.authenticate()
+        self.service = build('calendar', 'v3', credentials=creds)
+    
+    def authenticate(self):
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                self.creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                self.creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
