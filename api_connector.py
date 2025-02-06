52a93793f093d700143f145c2801b9d731e5dfe4
hannes <hannes.schniz@gmx.de>
2025-02-06 15:49:10 +0100

    initial commit

---
+++ api_connector.py
@@ -0,0 +1,49 @@
+import requests
+import environment
+
+class exporter:
+
+    classID = '3306'
+
+    url = "https://erato.webuntis.com/WebUntis/api/public/timetable/weekly/data"
+    options = "?elementType=1&elementId="+classID+"&date=2025-02-03&formatId=2"
+    
+    
+    headers = { 'accept': 'application/json', 
+    'cookie': environment.getCookie(),
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'}
+
+
+    def getElementMap(self, elements):
+        elementMap = {}
+        for element in elements:
+            elementMap[element['id']] = element
+            
+        return elementMap
+            
+
+    def getData(self):
+        
+
+        response = requests.get(self.url + self.options, headers=self.headers)
+
+
+        raw_data = response.json()['data']['result']['data']
+    
+        periods = raw_data['elementPeriods'][self.classID]
+        elements = raw_data['elements']
+        
+        elementMap = self.getElementMap(elements)
+    
+        for period in periods:
+            element_states = period['elements']
+            #elements.state [REGULAR|SUBSTITUTED]
+            #cellstate [CANCEL|STANDARD|ROOMSUBSTITUTION]
+            print('---------------------------------------')
+            print(elementMap[element_states[1]['id']]['name'])
+            print(elementMap[element_states[2]['id']]['name'])
+            print(str(period['date'])+":",period['startTime'], "->", period['endTime'])
+            
+exporterClass = exporter()
+
+exporterClass.getData()
