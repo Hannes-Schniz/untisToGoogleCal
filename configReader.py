36c9e5fdadbe32fdf87f6481b80c140c6545c7d0
hannes <hannes.schniz@gmx.de>
2025-02-07 07:44:23 +0100

    added error handling to config reader

--- configReader.py
+++ configReader.py
@@ -1,8 +1,26 @@
 import json
+import re
 
 
 class configExtract:
     conf = None
     def __init__(self):
         with open("config.json","r") as conf:
-           self.conf = json.load(conf) 
+           conf = json.load(conf)
+        try:
+            self.configCheck(conf) 
+            self.conf = conf
+        except Exception as e:
+            print(repr(e))
+            raise Exception("Failed to load config")
+        
+           
+           
+
+    def configCheck(self,conf):
+        if conf['group'] not in ["A","B"]:
+            raise Exception("Group not configured")
+        if re.match(conf['classID'], "[0-9]^4") == None or len(conf['classID']) != 4:
+            raise Exception("No valid class ID configured")
+        if re.match(conf['weeksAhead'], "[0-9]") == None or len(conf['weeksAhead']) != 1:
+            raise Exception("No valid weeks lookahead configured")
