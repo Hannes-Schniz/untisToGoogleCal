7ed084777043a1efc9068e377d646570b53fd8ef
hannes <hannes.schniz@gmx.de>
2025-04-24 19:33:36 +0200

    reenabled telegram bot

--- telegramBot.py
+++ telegramBot.py
@@ -9,7 +9,7 @@ chat_id = env.telegramChat
 def sendMessage(message):
     params = {"chat_id":chat_id,"text": message, "parse_mode": "HTML"}
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    #requests.post(url=url, params=params)
+    requests.post(url=url, params=params)
     
 def createText(summary, location, description, date, start, end ):
     date = f"{date.split('-')[2]}.{date.split('-')[1]}.{date.split('-')[0]}"
